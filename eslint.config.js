3c1909682236175537fbd5bcce82c1c569dbd900
hoanghuan2005 <hohuan2005uh@gmail.com>
2025-04-30 23:15:09 +0700

    material

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,52 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import react from "eslint-plugin-react";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react: react
     },
     rules: {
-      "react-refresh/only-export-components": "warn",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": 0,
-      "react/display-name": 0,
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 0,
+      'react/display-name': 0,
 
-      "no-console": 1,
-      "no-lonely-if": 1,
-      "no-unused-vars": 1,
-      "no-trailing-spaces": 1,
-      "no-multi-spaces": 1,
-      "no-multiple-empty-lines": 1,
-      "space-before-blocks": ["error", "always"],
-      "object-curly-spacing": [1, "always"],
-      "indent": ["warn", 2],
-      "semi": [1, "never"],
-      "quotes": ["error", "single"],
-      "array-bracket-spacing": 1,
-      "linebreak-style": 0,
-      "no-unexpected-multiline": "warn",
-      "keyword-spacing": 1,
-      "comma-dangle": 1,
-      "comma-spacing": 1,
-      "arrow-spacing": 1,
-    },
-  },
-];
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-unused-vars': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      'indent': ['warn', 2],
+      'semi': [1, 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
+    }
+  }
+]
